bef6dde04c53a4baf90d5ebf2aa68b1348dd93e1
오종철 <jongchul.oh@musinsa.com>
2021-02-24 08:10:38 +0900

    sed 명령 수정

--- til/til.sh
+++ til/til.sh
@@ -2,7 +2,7 @@ URL=`date "+%Y%m%d"`
 DATE=`date "+%Y.%m.%d"`
 echo $DATE
 cp -n _template.html $URL.html
-sed -ie 's/$date/'$DATE'/' $URL.html
-sed -ie 's/$url/'$URL'/' $URL.html
+sed -i '' 's/$date/'$DATE'/' $URL.html
+sed -i '' 's/$url/'$URL'/' $URL.html
 echo "COMPLETE!"
 
