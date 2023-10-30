c723f8bf53d7279503314eed91bfb9d34d8c4772
d.ohnald <77563784+dohnald@users.noreply.github.com>
2023-10-30 18:29:55 +0900

    Delete til directory

--- til/til.sh
+++
@@ -1,8 +0,0 @@
-URL=`date "+%Y%m%d"`
-DATE=`date "+%Y.%m.%d"`
-echo $DATE
-cp -n _template.html $URL.html
-sed -i '' 's/$date/'$DATE'/' $URL.html
-sed -i '' 's/$url/'$URL'/' $URL.html
-echo "COMPLETE!"
-
